441587e28fa543e17345e9d6b9bf760a8d6c650a
francojavled96 <francojavierledesma17@gmail.com>
2023-08-31 14:45:13 -0300

    Agrega clase Colaborador

---
+++ src/tp_2/Colaborador.java
@@ -0,0 +1,33 @@
+
+package tp_2;
+
+
+public class Colaborador {
+    private String nombre;
+    private String apellido;
+    
+    
+    public Colaborador ( String nombre, String apellido){
+    this.nombre= nombre;
+    this.apellido= apellido;
+    
+    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+}
